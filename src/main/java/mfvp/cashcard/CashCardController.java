b1c0e2c9124ef601dba5786c81584d3ee98607a6
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-23 09:13:12 -0300

    Configure the Database

--- src/main/java/mfvp/cashcard/CashCardController.java
+++ src/main/java/mfvp/cashcard/CashCardController.java
@@ -1,16 +1,28 @@
 package mfvp.cashcard;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        return cashCardOptional.map(cashCard -> ResponseEntity.status(HttpStatus.OK)
+                .body(cashCard))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
