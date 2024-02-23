811e3319e3f380f12e44573db8165ab72e5d5eaa
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-23 09:58:12 -0300

    Add the POST endpoint

--- src/main/java/mfvp/cashcard/CashCardController.java
+++ src/main/java/mfvp/cashcard/CashCardController.java
@@ -2,10 +2,7 @@ package mfvp.cashcard;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -24,5 +21,10 @@ public class CashCardController {
         return cashCardOptional.map(cashCard -> ResponseEntity.status(HttpStatus.OK)
                 .body(cashCard))
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard() {
+        return null;
     }
 }
