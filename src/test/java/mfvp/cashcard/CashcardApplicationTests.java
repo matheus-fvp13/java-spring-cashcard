52b4f2b76f5c440f4647b60c001684ffec0a51d9
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-23 09:54:04 -0300

    Test the HTTP POST Endpoint

--- src/test/java/mfvp/cashcard/CashcardApplicationTests.java
+++ src/test/java/mfvp/cashcard/CashcardApplicationTests.java
@@ -38,4 +38,11 @@ class CashcardApplicationTests {
         assertThat(response.getBody()).isBlank();
     }
 
+    @Test
+    void shouldCreatedANewCashCard() {
+        CashCard newCashCard = new CashCard(null, 250.00);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
 }
