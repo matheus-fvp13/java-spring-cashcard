8d740c8b5f7cd3cc2bb7640e15b1418da66a9f38
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-27 14:55:44 -0300

    Additional Testing and Spring Security's Influence

--- src/test/java/mfvp/cashcard/CashcardApplicationTests.java
+++ src/test/java/mfvp/cashcard/CashcardApplicationTests.java
@@ -160,6 +160,7 @@ class CashcardApplicationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    @DirtiesContext
     @Test
     void shouldUpdateAnExistingCashCard() {
         CashCard cashCardUpdate = new CashCard(null, 19.99, null);
@@ -180,4 +181,23 @@ class CashcardApplicationTests {
         assertThat(amount).isEqualTo(19.99);
     }
 
+    @Test
+    void shouldNotUpdateACashCardThatDoesNotExist() {
+        CashCard unknownCard = new CashCard(null, 19.99, null);
+        HttpEntity<CashCard> request = new HttpEntity<>(unknownCard);
+        ResponseEntity<Void> response = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .exchange("/cashcards/99999", HttpMethod.PUT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void shouldNotUpdateACashCardThatIsOwnedBySomeoneElse() {
+        CashCard kumarsCard = new CashCard(null, 333.33, null);
+        HttpEntity<CashCard> request = new HttpEntity<>(kumarsCard);
+        ResponseEntity<Void> response = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .exchange("/cashcards/102", HttpMethod.PUT, request, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 }
