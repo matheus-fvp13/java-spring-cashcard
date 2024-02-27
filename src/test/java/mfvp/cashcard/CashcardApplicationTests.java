d8e3d5c116f8befb7d7f3e3d26b4b1fd599ce6a3
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-27 15:56:31 -0300

    Enforce Ownership

--- src/test/java/mfvp/cashcard/CashcardApplicationTests.java
+++ src/test/java/mfvp/cashcard/CashcardApplicationTests.java
@@ -214,4 +214,12 @@ class CashcardApplicationTests {
                 .getForEntity("/cashcards/99", String.class);
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
+
+    @Test
+    void shouldNotDeleteACashCardThatDoesNotExist() {
+        ResponseEntity<Void> deleteResponse = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 }
